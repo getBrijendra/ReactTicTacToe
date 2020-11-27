c459471207491cbc08b9415b08a5adc4bbfb9170
= <=>
2020-11-27 18:19:00 +0530

    My First react app - tic tac toe

---
+++ src/Learnings.md
@@ -0,0 +1,14 @@
+//Create a default React App.
+npx create-react-app my-app
+
+//To start the server
+npm install
+npm start
+
+//Coding guidelines
+Choose immutability
+Naming conventions -- On[Event] - handle[Event]
+JSX syntax - Javascript XML like syntax.
+Babel for checking to what code compiles
+render() returns React elements.
+#Forgetting () => and writing onClick={function} is a common mistake, and would fire the alert every time the component re-renders.
--- src/index.js
+++ src/index.js
@@ -1,17 +1,171 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//class Square extends React.Component {
+function Square(props) {  //function components
+  //render() {
+    return (
+      <button 
+        className="square" 
+        onClick = {() => /*this.*/props.onClick()}
+      >
+        {/*this.*/props.value}
+      </button>
+    );
+  //}
+}
+
+class Board extends React.Component {
+  /*
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares : Array(9).fill(null), //Make Square a Controlled Components as its state maintained in Board
+      xIsNext : true,
+
+    };
+  }
+  */
+
+  renderSquare(i) {
+    return <Square 
+            value = {this.props.squares[i]}
+            onClick = {() => this.props.onClick(i)}  
+          />;
+  }
+
+  /*
+  handleClick(i) {
+    if(this.state.squares[i] != null || CalculateWinner(this.state.squares))
+      return;
+    const newsquare = this.state.squares.slice();     
+    //*
+      For immutability of the state 
+      Advantages: 
+        Complex features become simple.
+        Detecting changes.
+        Determining when to re-render in React.
+    /
+    newsquare[i] = this.state.xIsNext ? 'X' : 'O';    
+    this.setState({squares : newsquare, xIsNext : !this.state.xIsNext});
+  }
+  */
+
+  render() {
+    /*
+    let winner = CalculateWinner(this.state.squares);
+    let status;
+    if(winner === null)
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    else
+      status = 'Winner is : ' + winner;
+    */
+    return (
+      <div>
+         <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history : [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext : true,
+    }
+  }
+
+  handleClick(i) {
+    let history = this.state.history;
+    let currSquares = history[history.length -1]; 
+    if(currSquares.squares[i] != null || CalculateWinner(currSquares.squares))
+    return;
+    const newsquare = currSquares.squares.slice();     
+    /*
+      For immutability of the state 
+      Advantages: 
+        Complex features become simple.
+        Detecting changes.
+        Determining when to re-render in React.
+    */
+    newsquare[i] = this.state.xIsNext ? 'X' : 'O';    
+    history.push({squares : newsquare});
+    this.setState({history : history, xIsNext : !this.state.xIsNext});
+  }
+
+
+  render() {
+    let history = this.state.history;
+    let currSquares = history[history.length -1];
+
+    let winner = CalculateWinner(currSquares.squares);
+    let status;
+    if(winner === null)
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    else
+      status = 'Winner is : ' + winner;
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board 
+            squares = {currSquares.squares} 
+            //status = {status}
+            onClick = { (i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{ status }</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+function CalculateWinner(squares) {
+  const Arr = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6], 
+  ];
+
+  for(let i = 0; i < Arr.length; i++)
+  {
+    let [a, b, c] = Arr[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+      return squares[a];
+  }
+  return null;
+}
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
